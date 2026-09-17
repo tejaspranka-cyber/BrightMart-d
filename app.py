1bd5e49961f27857f90fadf768d7bf5c21965881
tejaspranka-cyber <tejaspranka@gmail.com>
2026-09-17 19:29:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
-import pickle
+import joblib
 import numpy as np
 
 # Load the saved model
-model = pickle.load(open('linear_reg.sav', 'rb'))
+model = joblib.load(open('linear_reg.sav', 'rb'))
 
 st.title('Sales Prediction App')
 
